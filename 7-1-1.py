85a14690154bf3031d693fad5294e54fccb4ab5a
M <whddnr3356@gmail.com>
2020-06-10 20:50:30 +0900

    2020-06-10 기숙사 coding!

--- 7-1-1.py
+++ 7-1-1.py
@@ -47,6 +47,61 @@ time.sleep(2)
 #페이지 내용
 #print('Before Page Contents= {}'.format(browser.page_source))
 
+#페이지 이동 시 새로운 데이터 수신 완료위한 대기 시간
+scroll_pause_time=4
+
+#현재 페이지 높이
+last_height=browser.execute_script('return document.documentElement.scrollHeight')
+
+print()
+
+#모든 댓글 데이터가 렌더링 완료되기 까지 반복
+while True:
+    # 스크롤바 이동
+    browser.execute_script('window.scrollTo(0,document.documentElement.scrollHeight)')
+
+    #대기
+    time.sleep(scroll_pause_time)
+
+    # 스크롤바 이동 -> 새로운 데이터 랜더링 -> 현재 높이를 구한다.
+    new_height=browser.execute_script('return document.documentElement.scrollHeight')
+
+    # 새로운 렌더링 없으면 종료
+    if new_height==last_height:
+        break
+
+    #높이 변경
+    last_height=new_height
+
+#bs4 초기화
+soup=BeautifulSoup(browser.page_source,'html.parser')
+
+#통계리스트
+top_level=soup.select('div#menu-container yt-formatted-string#text')
+#댓글리스트
+comment=soup.select('ytd-comment-renderer#comment')
+
+print('Total Like Count: {}'.format(top_level[0].text.strip()))
+print('Total Dislike Count: {}'.format(top_level[1].text.strip()))
+
+# Dom반복
+for dom in comment:
+    print()
+    #이미지 URL 정보
+    img_src=dom.select_one('#img').get('src')
+    print('Thumbnail Image URLS : {}'.format(img_src if img_src else 'None'))
+    #작성자
+    print('Author : {}'.format(dom.select_one('#author-text > span').text.strip()))
+    #댓글 본문
+    print('Author : {}'.format(dom.select_one('#content-text').text.strip()))
+    #좋아요
+    print('Vote Positive : {}'.format(dom.select_one('#vote-count-middle').text.strip()))
+
+
+
+
+
+
 
 
 #브라우저 종료
